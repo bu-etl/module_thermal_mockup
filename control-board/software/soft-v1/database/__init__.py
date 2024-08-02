972edae686aa6bad98db02f912ad281c0b297914
Hayden Swanson <hayden_swanson22@yahoo.com>
2024-08-02 05:27:03 -0400

    sqlalchemy models intitial

--- control-board/software/soft-v1/database/__init__.py
+++
@@ -1,1 +0,0 @@
-#sets up db connection
--- control-board/software/soft-v1/main.py
+++ control-board/software/soft-v1/main.py
@@ -1,1 +1,12 @@
 #GUI for control board
+
+from sqlalchemy import create_engine
+
+import env
+val = getattr(env, "DATABASE_URI")
+
+engine = create_engine(val, echo=True)
+
+print("hello world")
+
+ 
