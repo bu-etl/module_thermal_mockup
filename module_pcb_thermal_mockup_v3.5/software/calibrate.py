24baa6e25dc59615336611b103b21bdc051a6d79
Hayden Swanson <hayden_swanson22@yahoo.com>
2024-08-23 08:38:38 -0400

    Added logging across widgets via signal and slots

--- module_pcb_thermal_mockup_v3.5/software/calibrate.py
+++ module_pcb_thermal_mockup_v3.5/software/calibrate.py
@@ -1,12 +1,8 @@
 import sys
-import csv
-from collections import deque
 from datetime import datetime
 import PySide6.QtWidgets as qtw
-import PySide6.QtCharts as qtc
 from PySide6.QtSerialPort import QSerialPort, QSerialPortInfo
-from PySide6 import QtCore
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Signal, Slot, QIODevice, QTextStream
 from sqlalchemy import create_engine, select, Engine
 from sqlalchemy.orm import Session, scoped_session, sessionmaker
 from env import DATABASE_URI
@@ -20,10 +16,6 @@ ENABLED_CHANNELS = [1, 3, 8]
 DB_RUN_MODES = ('TEST', 'DEBUG', 'REAL')
 DATA_STORE = ['LOCAL', 'DB']
 
-#options:
-#keep communication and everything together
-#split it up by sensor
-
 channel_sensor_map = {
     1: 'E3',
     2: 'L1',
@@ -35,18 +27,35 @@ channel_sensor_map = {
     8: 'E4'
 }
 
-class Port:
-    def __init__(self):
+class CommPort(qtw.QComboBox):
+    log_message = Signal(str)  # Signal to propagate log messages
+
+    def __init__(self, log_callback=None):
+        super(CommPort, self).__init__()
         self.port = None
-        #self.all_ports = QSerialPortInfo.availablePorts() # make this go elsewhere
+        self.log_callback = log_callback  # Log callback to the main window log method
 
-        #add all_ports as options to the port layout
-        #set up action that when selected it makes self.port port info
+        self.clear()
+        self.addItem('Select Port')
+        for port_info in QSerialPortInfo.availablePorts():
+            self.addItem(port_info.portName(), port_info)
 
-    def connect(self, port):
-        self.disconnect()
+        #if the dropdown index changes
+        self.currentIndexChanged.connect(self.select_port) 
+
+    def select_port(self):
+        ''' Signal for when a port is selected from the dropdown'''
+        index = self.currentIndex()
+        if index > 0:  # Ignoring the first 'Select Port' line
+            port_info = self.itemData(index)
+            self.connect_port(port_info)
+
+    def connect_port(self, port):
+        """Connects and sets port to the corresponding port info"""
+        self.disconnect_port() #if already connected to another port, disconnect
         self.log(f"Connecting to port: {port.portName()}")
         self.port = QSerialPort(port)
+
         self.port.baudRate = QSerialPort.BaudRate.Baud9600
         #self.port.baudRate = QSerialPort.BaudRate.Baud115200
         self.port.breakEnabled = False
@@ -54,7 +63,7 @@ class Port:
         self.port.flowControl = QSerialPort.FlowControl.NoFlowControl
         self.port.parity = QSerialPort.Parity.NoParity
         self.port.stopBits = QSerialPort.StopBits.OneStop
-        if not self.port.open(QtCore.QIODevice.ReadWrite):
+        if not self.port.open(QIODevice.ReadWrite):
             print("Failed to open port!")
             self.port = None
             return
@@ -65,7 +74,7 @@ class Port:
         if self.port is None:
             return
         data = self.port.readLine()
-        data = QtCore.QTextStream(data).readAll().strip()
+        data = QTextStream(data).readAll().strip()
         return data
     
     def write(self, message: str):
@@ -73,38 +82,41 @@ class Port:
             return
         self.com_port.write(message.encode() + b'\n')
 
-    def disconnect(self):
+    def disconnect_port(self):
         if self.port is not None:
             if hasattr(self.port, '_error_handler'):
                 self.port.errorOccurred.disconnect(self.port._error_handler)
             self.port.close()
             self.port = None
 
+    def log(self, message):
+        ''' Emit log messages via signal '''
+        self.log_message.emit(message)
 
-#Widget for live readout graph (Not DB Saved)
-class OhmVsTimePlot(pg.PlotWidget):
-    def __init__(self):
-        super(OhmVsTimePlot, self).__init__()
+# #Widget for live readout graph (Not DB Saved)
+# class OhmVsTimePlot(pg.PlotWidget):
+#     def __init__(self):
+#         super(OhmVsTimePlot, self).__init__()
 
-    def update_plot(self):
-        #update plot
-        pass
+#     def update_plot(self):
+#         #update plot
+#         pass
 
-#Widget for each Sensor calib graph (DB Saved)
-class Sensor():
-    def __init__(self, channel: int, com_port):
-        #self.run_start_time = None
-        self.channel = channel if channel in ENABLED_CHANNELS else None
-        self.pending_readout = False
-        self.com_port = com_port
+# #Widget for each Sensor calib graph (DB Saved)
+# class Sensor():
+#     def __init__(self, channel: int, com_port: CommPort):
+#         #self.run_start_time = None
+#         self.channel = channel if channel in ENABLED_CHANNELS else None
+#         self.pending_readout = False
+#         self.com_port = com_port
 
-    def readout_adc(self):
-        self.write_port(f'measure {self.channel}')
-        self.pending_readout = True
+#     def readout_adc(self):
+#         self.write_port(f'measure {self.channel}')
+#         self.pending_readout = True
 
-class SubassemblyPlot(pg.PlotWidget):
-    def __init__(self):
-        super(SubassemblyPlot, self).__init__()
+# class SubassemblyPlot(pg.PlotWidget):
+#     def __init__(self):
+#         super(SubassemblyPlot, self).__init__()
 
         #somehow use Sensor to update and put both onto this plot widget, main makes 4 of these
 
@@ -115,18 +127,53 @@ class SubassemblyPlot(pg.PlotWidget):
 # -> Finish button
 
 class MainWindow(qtw.QMainWindow):
+    log_message = Signal(str)
+
     def __init__(self):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("Module Calibration") 
 
-        layout = qtw.QGridLayout()
+        self.menu = self.menuBar()
+        self.connect_menu = self.menu.addMenu('Connect')
 
-        layout.addWidget(SubassemblyPlot('Subassembly 1'), 0,0)
-        layout.addWidget(SubassemblyPlot('Subassembly 2'), 0,1)
-        layout.addWidget(SubassemblyPlot('Subassembly 3'), 1,0)
-        layout.addWidget(SubassemblyPlot('Subassembly 4'), 1,1)
+        self.comm_port= CommPort()
+        # - What an action is
+        #https://www.pythonguis.com/tutorials/pyside6-actions-toolbars-menus/
+        # - What QWidgetAction is
+        #https://doc.qt.io/qt-6/qwidgetaction.html
+        port_widget_action = qtw.QWidgetAction(self)
+        port_widget_action.setDefaultWidget(self.comm_port) #set the action for this widget
+        self.connect_menu.addAction(port_widget_action)
 
-        self.setCentralWidget(layout)
+        central_widget = qtw.QWidget()
+        main_layout = qtw.QVBoxLayout(central_widget)
 
+        self.serial_display = qtw.QPlainTextEdit()
+        self.serial_display.setReadOnly(True)
+        main_layout.addWidget(self.serial_display)
 
+        self.setCentralWidget(central_widget)
+
+        self.comm_port.log_message[str].connect(self.log)
+        # layout.addWidget(SubassemblyPlot('Subassembly 1'), 0,0)
+        # layout.addWidget(SubassemblyPlot('Subassembly 2'), 0,1)
+        # layout.addWidget(SubassemblyPlot('Subassembly 3'), 1,0)
+        # layout.addWidget(SubassemblyPlot('Subassembly 4'), 1,1)
+
+        
+    @Slot(str)
+    def log(self, text: str):
+        self.serial_display.appendPlainText(text)
+
+def main():
+    APP = qtw.QApplication()
+
+    window = MainWindow()
+    window.resize(800, 600)
+    window.show()
+
+    sys.exit(APP.exec())
+
+if __name__ == "__main__":
+    main()
