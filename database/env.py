441c16fa1068f7f1a77ecf582f48d67a0a6527dd
Hayden Swanson <hayden_swanson22@yahoo.com>
2024-12-08 11:22:57 -0500

    removing a big mistake...

--- database/env.py
+++
@@ -1,4 +0,0 @@
-DATABASE_URI = "postgresql://admin:8euTnKLaCCu@dbod-cms-etl-module-tm-db.cern.ch:6626/admin" 
-#DATABASE_URI = "postgresql://admin:8euTnKLaCCu@localhost:6626/admin" 
-
-#"postgresql://postgres:#MH6vMvjQ!M%gWqd#pn@localhost:5432/tm_mockup_temp"
